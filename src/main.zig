6b1910a0042a1b2d4ceaa1aadbe57bad06c02c0c
Maicon Santana <maiconpintoabreu@gmail.com>
2025-08-09 11:45:48 +0100

    set resizable to avoid fullscreen to keep the low resolution

--- src/main.zig
+++ src/main.zig
@@ -15,7 +15,7 @@ pub fn main() anyerror!void {
     const isFullScreen = buildin.os.tag == .windows or buildin.os.tag == .linux;
     const isBorderlessWindowed = buildin.mode != std.builtin.OptimizeMode.Debug;
     rl.setConfigFlags(rl.ConfigFlags{
-        .window_resizable = false,
+        .window_resizable = isFullScreen or isBorderlessWindowed,
     });
 
     if (gameController.initGame(false)) {
